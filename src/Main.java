2e022b907f0c8bba2a3193a8ad2866b782f0b2fa
Your Name <you@example.com>
2018-07-27 16:01:14 +0800

    first

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Object a = new Object();
+        a = 1;
+        System.out.println(a.getClass());
     }
 }
