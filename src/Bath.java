f180fa7b56a1f3b92a61bb38efe5fe0432816258
Your Name <you@example.com>
2018-07-27 17:59:29 +0800

    对象初始化示例

---
+++ src/Bath.java
@@ -0,0 +1,15 @@
+
+class Soap {
+    private String s;
+    Soap() {
+        System.out.println("Soap()");
+        s = "Constructor";
+    }
+    public String toString() {
+        return s;
+    }
+}
+
+
+public class Bath {
+}
