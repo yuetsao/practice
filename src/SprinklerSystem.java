4be2c47d762df8392a90802edadd964e3c04324e
Your Name <you@example.com>
2018-07-27 17:43:46 +0800

    2018/07/27练习代码

---
+++ src/SprinklerSystem.java
@@ -0,0 +1,34 @@
+
+class WarerSource {
+    private String s;
+    WarerSource() {
+        System.out.println("WarerSource()");
+        s = "Constructed";
+    }
+    public String toString() {
+        return s;
+    }
+}
+
+public class SprinklerSystem {
+    private String val1, val2,val3,val4;
+    private WarerSource warerSource = new WarerSource();
+    private int i;
+    private float f;
+    public String toString() {
+        return "val1=" + val1 + "\n"+
+               "val2=" + val2 + "\n"+
+               "val3=" + val3 + "\n"+
+               "val4=" + val4 + "\n"+
+                "i="   + i    + "\n"+
+                "f="   + f    + "\n"+
+                "warerSource=" + warerSource;
+    }
+
+    public static void main(String[] args) {
+        SprinklerSystem sprinklerSystem = new SprinklerSystem();
+        System.out.println(sprinklerSystem);
+    }
+}
+
+
