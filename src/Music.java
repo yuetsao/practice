7a2ed4886be75516249f41be6a3a5e664b034f41
Your Name <you@example.com>
2018-07-31 09:29:34 +0800

    show ploymohpism

---
+++ src/Music.java
@@ -0,0 +1,24 @@
+
+class Instruments {
+
+}
+
+class Winds extends Instruments {
+
+}
+
+class Percussion extends  Instruments {
+
+}
+
+class Stringed extends  Instruments {
+
+}
+
+class Brass extends Winds {
+
+}
+
+public class Music {
+
+}
