366b3190b016b5bfd73124b4be274c2abb88b3f3
Your Name <you@example.com>
2018-08-02 17:54:49 +0800

    内部类的创建 外部类引用.new

---
+++ src/Parcel3.java
@@ -0,0 +1,24 @@
+
+public class Parcel3 {
+    class Contents {
+        private int i = 11;
+        public int value() {return i;}
+    }
+    class Destination {
+        private String label;
+        Destination (String whereTo) {
+            label = whereTo;
+        }
+        String readLabel() {
+            return label;
+        }
+    }
+
+    public static void main(String[] args) {
+        Parcel3 parcel3 = new Parcel3();
+        Parcel3.Contents contents = parcel3.new Contents();
+        Parcel3.Destination destination = parcel3.new Destination("Maria");
+        System.out.println(destination.readLabel());
+    }
+
+}
