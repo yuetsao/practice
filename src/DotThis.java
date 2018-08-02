f62f12641a1c6f270860a07e4b99c993f692f292
Your Name <you@example.com>
2018-08-02 17:21:54 +0800

    内部类获得外部类的引用 外部类.this

---
+++ src/DotThis.java
@@ -0,0 +1,20 @@
+
+public class DotThis {
+    void f() {
+        System.out.println("DotThis.f()");
+    }
+    public class Inner {
+        public DotThis outer () {
+            return DotThis.this;
+        }
+    }
+    public Inner inner () {
+        return new Inner();
+    }
+
+    public static void main(String[] args) {
+        DotThis dt = new DotThis();
+        DotThis.Inner inner = dt.inner();
+        inner.outer().f();
+    }
+}
