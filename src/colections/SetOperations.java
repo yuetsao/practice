0f6959306ec59567a54a3167c34213351146dd06
caoyue <yuetsao@foxmail.com>
2018-08-29 17:28:49 +0800

    colections4

---
+++ src/colections/SetOperations.java
@@ -0,0 +1,14 @@
+package colections;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOperations {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<String>();
+        Collections.addAll(set."A B C D E L".split(" "));
+        set.add("M");
+
+    }
+}
