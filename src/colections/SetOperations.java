0297a8c6f76d74ad8f3f9016038b1a05165a0318
caoyue <yuetsao@foxmail.com>
2018-08-29 17:29:35 +0800

    colections4

--- src/colections/SetOperations.java
+++ src/colections/SetOperations.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class SetOperations {
     public static void main(String[] args) {
         Set<String> set = new HashSet<String>();
-        Collections.addAll(set."A B C D E L".split(" "));
+        Collections.addAll(set,"A B C D E L".split(" "));
         set.add("M");
 
     }
