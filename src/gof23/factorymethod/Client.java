c7501d6ad8d0152eda26db7ee6e6a7e6926ee8ce
caoyue <yuetsao@foxmail.com>
2018-12-17 15:49:21 +0800

    练习

--- src/gof23/factorymethod/Client.java
+++ src/gof23/factorymethod/Client.java
@@ -1,6 +1,7 @@
 package gof23.factorymethod;
 
 public class Client {
+    //虽然满足了设计模式但是类变多了
     public static void main(String[] args) {
         Car audi = new AudiFactory().creatCar();
         Car byd = new BydFactory().creatCar();
