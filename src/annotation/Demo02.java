8f1351165001ce483db1006a115c65ca22504a2c
caoyue <yuetsao@foxmail.com>
2018-12-20 14:26:21 +0800

    注解

---
+++ src/annotation/Demo02.java
@@ -0,0 +1,15 @@
+package annotation;
+
+//@CyAnnotation
+public class Demo02 {
+
+    @CyAnnotation(age = 19, studentName = "cy", arr = {"bijing", "qinghua"}, id = 0)
+    public void test() {
+
+    }
+
+//    @CyAnnotation(aa)
+    public void test1() {
+
+    }
+}
