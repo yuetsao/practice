c42a565b2639fb25453f60f2aaf3a0c268843f43
caoyue <yuetsao@foxmail.com>
2018-12-20 15:50:57 +0800

    Class对象

--- src/reflections/Demo01.java
+++ src/reflections/Demo01.java
@@ -11,6 +11,7 @@ public class Demo01 {
             //对象是表示或者封装一些数据。一个类被加载后，jvm会创建一个对应的Class对象，类的整个结构信息会放到Class对象中
             //这个Class对象就像一面镜子，通过这个Class对象就可以看到这个项目的整个结构信息
             //一个类只有一个Class对象，所以hashCode相同
+            //数组的class和维度有关系
             System.out.println(clazz);
             System.out.println(clazz.hashCode());
             Class<String> stringClass = String.class;
