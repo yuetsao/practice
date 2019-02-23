ac7699fd2af65c44fbbd17e2d7bcfaadfc7a74d1
caoyue <yuetsao@foxmail.com>
2019-02-23 16:17:25 +0800

    深入剖析tomcat源代码

---
+++ src/tomcat/ex03/pyrmont/startup/Bootstrap.java
@@ -0,0 +1,10 @@
+package tomcat.ex03.pyrmont.startup;
+
+import tomcat.ex03.pyrmont.connector.http.HttpConnector;
+
+public final class Bootstrap {
+  public static void main(String[] args) {
+    HttpConnector connector = new HttpConnector();
+    connector.start();
+  }
+}
---
+++ src/tomcat/ex20/pyrmont/standardmbeantest/CarMBean.java
@@ -0,0 +1,8 @@
+package tomcat.ex20.pyrmont.standardmbeantest;
+public interface CarMBean {
+
+  public String getColor();
+  public void setColor(String color);
+  public void drive();
+
+}
