5ec66d495a539e464acb9c52334870c934a094f6
caoyue <yuetsao@foxmail.com>
2019-12-06 10:09:48 +0800

    concurentcy

---
+++ src/main/java/aspectj/MyAspectJDemo.java
@@ -0,0 +1,32 @@
+package aspectj;
+
+/**
+ * @author caoyue
+ * @description 大概描述所属模块和介绍
+ * @date 2019-10-24 11:05
+ **/
+public class MyAspectJDemo {
+//    /**
+//     * 定义切点,日志记录切点
+//     */
+//    pointcut recordLog():call(* HelloWord.sayHello(..));
+//
+//    /**
+//     * 定义切点,权限验证(实际开发中日志和权限一般会放在不同的切面中,这里仅为方便演示)
+//     */
+//    pointcut authCheck():call(* HelloWord.sayHello(..));
+//
+//    /**
+//     * 定义前置通知!
+//     */
+//    before():authCheck(){
+//        System.out.println("sayHello方法执行前验证权限");
+//    }
+//
+//    /**
+//     * 定义后置通知
+//     */
+//    after():recordLog(){
+//        System.out.println("sayHello方法执行后记录日志");
+//    }
+}
