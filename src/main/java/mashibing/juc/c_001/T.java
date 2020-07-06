6ce1cbc845b69690621b00cd246e156cd7075be4
yuetsao <yuetsao@foxmail.com>
2020-07-06 17:25:46 +0800

    feat:volatile保证线程可见性和和禁止指令重排序

--- src/main/java/mashibing/juc/c_001/T.java
+++ src/main/java/mashibing/juc/c_001/T.java
@@ -1,7 +1,7 @@
 package mashibing.juc.c_001;
 /**
  * @Title: T
- * @Description: TODO(用一句话描述该文件做什么)
+ * @Description:  
  * @author: caoyue
  * @date: 2020/7/6 17:02
  * @最后修改人: caoyue
@@ -16,16 +16,17 @@ package mashibing.juc.c_001;
  * @Description:
  */
 public class T implements Runnable {
-    private /**volatile**/  int count = 100;
+    private volatile  int count = 100;
     
     @Override
     public synchronized void run() {
-        count --;
+        synchronized (this){count --;}
         System.out.println(Thread.currentThread().getName() + "count "+ count);
     }
     public static void main(String[] args) {
-        T t = new T();
+                    T t = new T();       
         for(int i=0; i<10; i++) {
+//            T t = new T();
             new Thread(t, "THREAD"+ i).start();
         }
     }
